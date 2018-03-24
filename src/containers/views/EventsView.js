1e3749ee91547e6485c8e88c2478ff883cba0f83
“Anton <antonbykovskih1@gmail.com>
2018-03-25 01:36:18 +0200

    Add modal, starting to work on sorting

--- src/containers/views/EventsView.js
+++ src/containers/views/EventsView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native';
 import rest from '../../utils/rest';
 import { connect } from 'react-redux';
 import { IconImage } from '../../components/Layout/Layout';
@@ -9,6 +9,7 @@ import EventsList from '../../components/Events/EventsList';
 const mapStateToProps = state => ({
   events: state.events,
   auth: state.auth,
+  changeOrder: state.changeOrder,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -18,6 +19,9 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class EventsView extends Component {
+  state = {
+    changeOrder: false,
+  };
   static navigationOptions = {
     title: 'Events',
     header: {
@@ -43,12 +47,13 @@ class EventsView extends Component {
     const { events } = this.props;
 
     if (!events.loading) {
-      console.log('EVEEEENTS___', events);
       return <EventsList events={events} />;
     }
 
     return <ActivityIndicator />;
   };
+
+  changeSortOrder = () => {};
 
   render = () => {
     if (!this.props.auth.data.decoded) {
@@ -62,6 +67,9 @@ class EventsView extends Component {
     return (
       <View style={{ flex: 1 }}>
         <EventsHeader headerText="Events" />
+        <TouchableOpacity onPress={() => this.changeSortOrder()}>
+          <Text> Recommended </Text>
+        </TouchableOpacity>
         {this.renderContent()}
       </View>
     );
